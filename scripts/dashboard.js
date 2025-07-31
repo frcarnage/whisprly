3274329a7748cf32331dc65ab6ff231353c09686
frcarnage <vinitjha2712@gmail.com>
2025-07-31 23:46:56 +0530

    Delete scripts directory

--- scripts/dashboard.js
+++
@@ -1,8 +0,0 @@
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
-
-const auth = getAuth();
-document.getElementById("logoutBtn").addEventListener("click", () => {
-  signOut(auth).then(() => {
-    window.location.href = "/admin/login.html";
-  });
-});

a90f66853f9c85a61a4cd8286fe1adcabb7f066e
frcarnage <vinitjha2712@gmail.com>
2025-08-01 10:12:12 +0530

    Update firebase-init.js

--- scripts/firebase-init.js
+++ scripts/firebase-init.js
@@ -3,18 +3,18 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// TODO: Replace with your actual Firebase config
+// ✅ Correct Firebase config for Whisprly
 const firebaseConfig = {
   apiKey: "AIzaSyAGJntf2pacqzkhyvj7P80NsNQkSAGX29g",
   authDomain: "whisprly-5415a.firebaseapp.com",
-  databaseURL: "https://whisprly-5415a-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "whisprly-5415a",
-  storageBucket: "whisprly-5415a.firebasestorage.app",
+  storageBucket: "whisprly-5415a.appspot.com", // fixed
   messagingSenderId: "269867240239",
   appId: "1:269867240239:web:377d0a7c3d1c3870c7efda",
-  measurementId: "G-2CXWX5ZWH5
+  measurementId: "G-2CXWX5ZWH5" // fixed quote
 };
 
+// ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);

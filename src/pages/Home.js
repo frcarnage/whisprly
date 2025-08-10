91988db69af51f5cc42dff9c1831620fa73bae01
frcarnage <vinitjha2712@gmail.com>
2025-08-10 22:06:48 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:36}}>
+      <h1>Welcome to Whisprly!</h1>
+      <p>This is the user home. Add your web app features for users here.</p>
+    </div>
+  );
+}

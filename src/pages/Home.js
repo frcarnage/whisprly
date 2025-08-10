7a9b384619b76cd7b732a7abc0e740a96dfb678c
frcarnage <vinitjha2712@gmail.com>
2025-08-10 22:14:55 +0530

    Delete src directory

--- src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:36}}>
-      <h1>Welcome to Whisprly!</h1>
-      <p>This is the user home. Add your web app features for users here.</p>
-    </div>
-  );
-}

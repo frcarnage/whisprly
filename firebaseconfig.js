0c0db9755c0386245056c30310c3d1cc86b8a7da
frcarnage <vinitjha2712@gmail.com>
2025-08-12 11:29:23 +0530

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,14 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGJntf2pacqzkhyvj7P80NsNQkSAGX29g",
+  authDomain: "whisprly-5415a.firebaseapp.com",
+  databaseURL: "https://whisprly-5415a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "whisprly-5415a",
+  storageBucket: "whisprly-5415a.firebasestorage.app",
+  messagingSenderId: "269867240239",
+  appId: "1:269867240239:web:377d0a7c3d1c3870c7efda",
+  measurementId: "G-2CXWX5ZWH5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

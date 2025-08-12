a54ca95ecb3c50e1059a8b7b88b653ecf84160fe
frcarnage <vinitjha2712@gmail.com>
2025-08-12 11:52:32 +0530

    Create firebaseconfig.js

---
+++ js/firebaseconfig.js
@@ -0,0 +1,27 @@
+// /js/firebaseconfig.js
+
+// Import functions from v10 modular SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGJntf2pacqzkhyvj7P80NsNQkSAGX29g",
+  authDomain: "whisprly-5415a.firebaseapp.com",
+  databaseURL: "https://whisprly-5415a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "whisprly-5415a",
+  storageBucket: "whisprly-5415a.firebasestorage.app",
+  messagingSenderId: "269867240239",
+  appId: "1:269867240239:web:377d0a7c3d1c3870c7efda",
+  measurementId: "G-2CXWX5ZWH5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export initialized services for use in other files
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const rtdb = getDatabase(app);
